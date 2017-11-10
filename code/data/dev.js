d9e10797f8638f1eace0d8be6bd83a9ff8c4370a
Andres Arias <aariasc@gmail.com>
2017-11-10 14:00:28 -0700

    Initial nightwatch configuration, more NPM packages, data file, docker-compose file with sample environment

---
+++ code/data/dev.js
@@ -0,0 +1,4 @@
+module.exports = {
+  publicSiteUrl:'http://172.18.0.3:8080',
+  adminSiteUrl:'http://172.18.0.3:8080/wp-login.php'
+};
---
+++ code/nightwatch.js
@@ -0,0 +1,40 @@
+module.exports = {
+    src_folders: ['./tests'],
+    output_folder: './results',
+    live_output: true,
+    custom_commands_path: './commands',
+    custom_assertions_path:'./assertions',
+    page_objects_path: './pages',
+
+    test_settings: {
+      default: {
+        launch_url: 'http://localhost',
+        selenium_host: '127.0.0.1',
+        selenium_port: 4444,
+        silent: true,
+        disable_colors: false,
+        screenshots: {
+          enabled: true,
+          path: './results/screenshots'
+        },
+        desiredCapabilities: {
+          browserName: 'chrome',
+          javascriptEnabled: true,
+          acceptSslCerts: true
+        },
+        globals: require('./data/dev')
+      },
+      local_chrome:{
+        selenium: {
+          start_process: true,
+          server_path: './node_modules/selenium-server-standalone-jar/jar/selenium-server-standalone-3.4.0.jar',
+          log_path: './results',
+          host: '127.0.0.1',
+          port: 4444,
+          cli_args: {
+            'webdriver.chrome.driver': './node_modules/.bin/chromedriver'
+          }
+        }
+      }
+    }
+  };
